b3bde667a5feadc0993abed75241cf9f3d10d850
faizrahim <faizrahimbusiness@gmail.com>
2023-01-14 12:18:56 +0530

    add data

--- Test/datagen_test.py
+++ Test/datagen_test.py
@@ -7,11 +7,11 @@ from numpy import asarray
 cap = cv2.VideoCapture(0)
 start = False
 data_type = "train"
-current_class = "nID"
+current_class = "yID"
 dir_ = f"data/{data_type}/{current_class}"
 
 available_samples = len(os.listdir(dir_))
-total_samples_gen = 100
+total_samples_gen = 600
 
 
 x, y, w, h =  400, 30, 600,600
@@ -55,6 +55,7 @@ while not start:
 
 for i in range(available_samples, total_samples_gen):
 	ret, shot = cap.read()
+	time.sleep(0.07)
 	print(i,"/",total_samples_gen)
 	if not ret:
 		continue
@@ -62,22 +63,22 @@ for i in range(available_samples, total_samples_gen):
 	cv2.rectangle(shot, (x, y), (x + w, y + h), (0, 0, 255), 1)
 	cv2.putText(shot,
 				f"Capturing {current_class}", (x, y - 5),
-				cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 0, 0), 1)
+				cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 0), 1)
 	cv2.putText(shot,
 				f"class:{current_class}",
 				(x, y + h + 10),
 				cv2.FONT_HERSHEY_DUPLEX,
-				0.4, (255, 0, 0), 1)
+				0.4, (0, 255, 255), 1)
 	cv2.putText(shot,
 				f"type:{data_type}",
 				(x, y + h + 20),
 				cv2.FONT_HERSHEY_DUPLEX,
-				0.4, (255, 0, 0), 1)
+				0.4, (0, 255, 255), 1)
 	cv2.putText(shot,
 				f"samples captured:{i}/{total_samples_gen}",
 				(x, y + h + 30),
 				cv2.FONT_HERSHEY_DUPLEX,
-				0.4, (255, 0, 0), 1)
+				0.4, (0, 255, 255), 1)
 
 	roi = shot[y:y+h,x:x+w]
 	cv2.imwrite(f"{dir_}/{i}.png",roi)
